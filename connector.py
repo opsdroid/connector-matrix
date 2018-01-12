2c6ab015d4b63dd24cda7934bc38ae61fa009e68
Drew Leonard <andy.j.leonard@gmail.com>
2018-01-12 18:22:27 +0000

    Reinstate fallback of single room
    If only one is specified in the config

--- connector.py
+++ connector.py
@@ -17,7 +17,9 @@ class ConnectorMatrix(Connector):
         # Init the config for the connector
         self.name = "ConnectorMatrix"  # The name of your connector
         self.config = config  # The config dictionary to be accessed later
-        self.rooms = config['rooms']
+        self.rooms = config.get('rooms', None)
+        if not self.rooms:
+            self.rooms = {'main': config['room']}
         self.room_ids = {}
         self.default_room = self.rooms['main']
         self.mxid = config['mxid']
