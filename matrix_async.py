13d72309dfc27201bd55c808032b5065295f67c9
Stuart Mumford <stuart@cadair.com>
2018-04-10 18:56:06 +0100

    fix retry exception handling

--- matrix_async.py
+++ matrix_async.py
@@ -64,12 +64,11 @@ class AsyncHTTPAPI(MatrixHttpApi):
                 data=content,
                 headers=headers)
             async with request as response:
-                if response.status < 200 or response.status >= 300:
-                    raise MatrixRequestError(
-                        code=response.status, content=await response.text())
-
                 if response.status == 429:
                     await sleep(response.json()['retry_after_ms'] / 1000)
+                elif response.status < 200 or response.status >= 300:
+                    raise MatrixRequestError(
+                        code=response.status, content=await response.text())
                 else:
                     return await response.json()
 
