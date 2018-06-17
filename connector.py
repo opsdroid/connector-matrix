57c74d4cc90b7eea1327d14a47bdabb75691c059
Stuart Mumford <stuart@cadair.com>
2018-06-17 17:55:23 +0100

    strip start and end p tags

--- connector.py
+++ connector.py
@@ -155,8 +155,13 @@ class ConnectorMatrix(Connector):
         "org.matrix.custom.html" format
         """
         clean_markdown = clean(message)
+        _LOGGER.debug(clean_markdown)
         html = markdown.markdown(message)
         clean_html = clean(html)
+        if clean_html.startswith('<p>'):
+            clean_html = clean_html[3:]
+            if clean_html.endswith('</p>'):
+                clean_html = clean_html[:-4]
         return {
             "body": body if body else re.sub('<[^<]+?>', '', clean_markdown),
             "msgtype": msgtype,
